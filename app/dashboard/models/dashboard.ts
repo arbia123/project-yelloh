16cc59293a0aa3c9217a932bb2717eaa7c81f819
arbia123 <43918273+arbia123@users.noreply.github.com>
2019-08-20 16:33:54 +0100

    clean-rebase

--- app/dashboard/models/dashboard.ts
+++
@@ -1,9 +0,0 @@
-import { WidgetList } from "./widgetList";
-import { User } from "./user";
-export interface Dashboard {
-  id: string;
-  name: string;
-  user: User;
-  widgets: Array<WidgetList>;
-}
-
--- app/dashboard/models/user.ts
+++
@@ -1,5 +0,0 @@
-export class User {
-    public id: string;
-    public firstName: string;
-    public lastName: string;
-  }
